3460c4880cdcef693b590975bd9367a9b3f72460
chickenblock <chickenblock24@gmail.com>
2024-04-14 12:07:38 +0530

    initial implementation for custom Signature Algorithm complete

--- crypto/algorithms/src/main/java/org/hyperledger/besu/crypto/SatschainNosigner.java
+++ crypto/algorithms/src/main/java/org/hyperledger/besu/crypto/SatschainNosigner.java
@@ -8,6 +8,9 @@ import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.bytes.MutableBytes;
 import org.apache.tuweni.units.bigints.UInt256;
+import org.bouncycastle.asn1.sec.SECNamedCurves;
+import org.bouncycastle.asn1.x9.X9ECParameters;
+import org.bouncycastle.crypto.params.ECDomainParameters;
 import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.math.ec.rfc8032.Ed25519.Algorithm;
 
@@ -23,21 +26,35 @@ import org.bouncycastle.math.ec.rfc8032.Ed25519.Algorithm;
 public class SatschainNosigner implements SignatureAlgorithm {
 
   private final static String ALGORITHM_NAME = "satschain_nosigner";
-  private final static BigInteger ORDER = BigInteger.valueOf(1).shiftLeft(256);
+  protected final ECDomainParameters curve;
+  private final BigInteger curveOrder;
+  private final BigInteger halfCurveOrder;
 
+  public SatschainNosigner() {
+    final X9ECParameters params = SECNamedCurves.getByName("secp256k1");
+    curve = new ECDomainParameters(params.getCurve(), params.getG(), params.getN(), params.getH());
+    curveOrder = curve.getN();
+    halfCurveOrder = curveOrder.shiftRight(1);
+  }
+
+  @Override
   public void disableNative(){}
 
+  @Override
   public boolean isNative(){return false;}
 
+  @Override
   public SECPSignature sign(final Bytes32 dataHash, final KeyPair keyPair) {
-    return SECPSignature.create(new BigInteger(1, Bytes.random(32).toArray()), new BigInteger(1, keyPair.getPublicKey().getEncodedBytes().slice(0,32).toArray()), (byte)0, ORDER);
+    return SECPSignature.create(new BigInteger(1, Bytes.random(32).toArray()), new BigInteger(1, keyPair.getPublicKey().getEncodedBytes().slice(0,32).toArray()), (byte)0, curveOrder);
   }
 
   // verify passes if the s of the signature matches the public key
+  @Override
   public boolean verify(final Bytes data, final SECPSignature signature, final SECPPublicKey pub) {
     return signature.getS().equals(new BigInteger(1, pub.getEncodedBytes().slice(0, 32).toArray()));
   }
 
+  @Override
   public boolean verify(
       final Bytes data,
       final SECPSignature signature,
@@ -46,93 +63,111 @@ public class SatschainNosigner implements SignatureAlgorithm {
     return signature.getS().equals(new BigInteger(1, pub.getEncodedBytes().slice(0, 32).toArray()));
   }
 
+  @Override
   public SECPSignature normaliseSignature(
       final BigInteger nativeR,
       final BigInteger nativeS,
       final SECPPublicKey publicKey,
       final Bytes32 dataHash) {
-    return SECPSignature.create(nativeR, nativeS, (byte)(0), ORDER);
+    return SECPSignature.create(nativeR, nativeS, (byte)(0), curveOrder);
   }
 
   /**
-   * Calculate ecdh key agreement as bytes32.
-   *
-   * @param privKey the private key
-   * @param theirPubKey the public key
-   * @return the bytes 32
+   * Don't know what this function is used for for now returning a 0
    */
-  Bytes32 calculateECDHKeyAgreement(final SECPPrivateKey privKey, final SECPPublicKey theirPubKey);
+  @Override
+  public Bytes32 calculateECDHKeyAgreement(final SECPPrivateKey privKey, final SECPPublicKey theirPubKey) {
+    return Bytes32.ZERO;
+  }
 
+  @Override
   public BigInteger getHalfCurveOrder(){
-    return ORDER.shiftRight(1);
+    return halfCurveOrder;
   }
 
+  @Override
   public String getProvider() {
-    return ALGORITHM;
+    return ALGORITHM_NAME;
   }
 
+  @Override
   public String getCurveName() {
-    return ALGORITHM;
+    return ALGORITHM_NAME;
   }
 
+  @Override
   public SECPPrivateKey createPrivateKey(final BigInteger key) {
-    return SECPPrivateKey.create(key, ALGORITHM);
+    return SECPPrivateKey.create(key, ALGORITHM_NAME);
   }
 
+  @Override
   public SECPPrivateKey createPrivateKey(final Bytes32 key) {
-    return SECPPrivateKey.create(key, ALGORITHM);
+    return SECPPrivateKey.create(key, ALGORITHM_NAME);
   }
 
+  @Override
   public SECPPublicKey createPublicKey(final SECPPrivateKey privateKey) {
     MutableBytes pb = MutableBytes.create(64);
     privateKey.getEncodedBytes().copyTo(pb, 0);
     privateKey.getEncodedBytes().copyTo(pb, 32);
-    return SECPPublicKey.create(pb, ALGORITHM);
+    return SECPPublicKey.create(pb, ALGORITHM_NAME);
   }
 
+  @Override
   public SECPPublicKey createPublicKey(final BigInteger key) {
-    return SECPPublicKey.create(key, ALGORITHM);
+    return SECPPublicKey.create(key, ALGORITHM_NAME);
   }
 
+  @Override
   public SECPPublicKey createPublicKey(final Bytes encoded) {
-    return SECPPublicKey.create(encoded, ALGORITHM);
+    return SECPPublicKey.create(encoded, ALGORITHM_NAME);
   }
 
+  @Override
   public Optional<SECPPublicKey> recoverPublicKeyFromSignature(
       final Bytes32 dataHash, final SECPSignature signature) {
       MutableBytes pb = MutableBytes.create(64);
       UInt256.valueOf(signature.getS()).copyTo(pb, 0);
       UInt256.valueOf(signature.getS()).copyTo(pb, 32);
-      return Optional.of(SECPPublicKey.create(pb, ALGORITHM));
+      return Optional.of(SECPPublicKey.create(pb, ALGORITHM_NAME));
   }
 
-  ECPoint publicKeyAsEcPoint(final SECPPublicKey publicKey);
+  @Override
+  public ECPoint publicKeyAsEcPoint(final SECPPublicKey publicKey) {
+    return curve.getCurve().createPoint(new BigInteger(1, publicKey.getEncodedBytes().slice(0,32).toArray()), new BigInteger(1, publicKey.getEncodedBytes().slice(32,32).toArray()));
+  }
 
-  public boolean isValidPublicKey(SECPPublicKey publicKey) {
+  @Override
+  public boolean isValidPublicKey(final SECPPublicKey publicKey) {
     Bytes b1 = publicKey.getEncodedBytes().slice(0,32);
     Bytes b2 = publicKey.getEncodedBytes().slice(32,32);
     return b1.equals(b2);
   }
 
+  @Override
   public KeyPair createKeyPair(final SECPPrivateKey privateKey) {
     return new KeyPair(privateKey, createPublicKey(privateKey));
   }
 
+  @Override
   public KeyPair generateKeyPair() {
     Bytes32 seed = Bytes32.random();
     SECPPrivateKey privateKey = createPrivateKey(seed);
     return new KeyPair(privateKey, createPublicKey(privateKey));
   }
 
+  @Override
   public SECPSignature createSignature(final BigInteger r, final BigInteger s, final byte recId){
-    return SECPSignature.create(r, s, recId, ORDER);
+    return SECPSignature.create(r, s, recId, curveOrder);
   }
 
+  @Override
   public SECPSignature decodeSignature(final Bytes bytes) {
-    return SECPSignature.decode(bytes, ORDER);
+    return SECPSignature.decode(bytes, curveOrder);
   }
 
+  @Override
   public Bytes compressPublicKey(final SECPPublicKey uncompressedKey) {
-    return SECPPublicKey.create(uncompressedKey.getEncodedBytes(), ALGORITHM).getEncodedBytes();
+    return SECPPublicKey.create(uncompressedKey.getEncodedBytes(), ALGORITHM_NAME).getEncodedBytes();
   }
 }
